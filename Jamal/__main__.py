f71a2eb8f40cfbf9d3d47bf7551d9704f1058b68
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 02:24:38 +0700

    Create __main__.py

---
+++ Jamal/__main__.py
@@ -0,0 +1,47 @@
+import asyncio
+import signal
+
+import tornado.ioloop
+import tornado.platform.asyncio
+
+from pyrogram import idle
+from pyrogram.errors import UserDeactivated
+
+from Jamal import *
+from Jamal.database import *
+
+async def main():
+    await bot.start()
+    for _ubot in await get_userbots():
+        ubot_ = Ubot(**_ubot)
+        try:
+            await asyncio.wait_for(ubot_.start(), timeout=10)
+            await ubot_.join_chat("newhiganbana")
+        except asyncio.TimeoutError:
+            print(f"[𝗜𝗡𝗙𝗢]: {int(_ubot['name'])} 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
+        except Exception as e:
+            await remove_ubot(int(_ubot["name"]))
+            await rem_expired_date(int(_ubot["name"]))
+            print(f"[𝗜𝗡𝗙𝗢]: {int(_ubot['name'])}\n{e}")
+        except UserDeactivated:
+            await remove_ubot(int(_ubot["name"]))
+            await rem_expired_date(int(_ubot["name"]))
+            await remove_all_vars(int(_ubot["name"]))
+            await remove_from_vars(bot.me.id, "PREM_USERS", int(_ubot["name"]))
+            print(f"[ INFO ]: {int(_ubot['name'])} akun terhapus dibersihkan")
+    await asyncio.gather(loadPlugins(), installPeer(), expiredUserbots(), idle())
+    stop_event = asyncio.Event()
+    loop = asyncio.get_running_loop()
+    for s in (signal.SIGINT, signal.SIGTERM):
+        loop.add_signal_handler(s, lambda: asyncio.create_task(shutdown(s, loop)))
+    try:
+        await stop_event.wait()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await bot.stop()
+
+if __name__ == "__main__":
+    tornado.platform.asyncio.AsyncIOMainLoop().install()
+    loop = tornado.ioloop.IOLoop.current().asyncio_loop
+    loop.run_until_complete(main())
