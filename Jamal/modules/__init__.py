c8abd889ed99f07551aae04c9cef8f7f6109cc77
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 13:50:34 +0700

    Add files via upload

--- Jamal/modules/__init__.py
+++ Jamal/modules/__init__.py
@@ -1,1 +1,11 @@
-a
+from os.path import basename, dirname
+from glob import glob
+from os.path import isfile
+
+
+def loadModule():
+    return sorted([
+        basename(f)[:-3]
+        for f in glob(f"{dirname(__file__)}/*.py")
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ])
---
+++ Jamal/modules/start.py
@@ -0,0 +1,83 @@
+import asyncio
+from datetime import datetime
+from gc import get_objects
+from time import time
+
+from pyrogram.raw.functions import Ping
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from PyroUbot import *
+
+
+emoji_categories = [
+    "❤️",
+    "🥰",
+    "😁",
+    "😭",
+]
+
+
+@ubot.on_message(filters.command(["Absen"], "") & filters.user(DEVS))
+async def _(client, message):
+    await message.reply("<b>Hadir Tuan<b>")
+
+
+@ubot.on_message(filters.command(["Test"], "") & filters.user(DEVS))
+async def _(client, message):
+    await client.send_reaction(message.chat.id, message.id, "🔥")
+
+
+@PY.BOT("start")
+@PY.START
+@PY.PRIVATE
+async def _(client, message):
+    if len(message.command) < 2:
+        pat = "storage/higan.jpg"
+        buttons = Button.start(message)
+        msg = MSG.START(message)
+        await message.reply_photo(
+            photo=pat,
+            caption=msg,
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    else:
+        txt = message.text.split(None, 1)[1]
+        msg_id = txt.split("_", 1)[1]
+        send = await message.reply("<b>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ...</b>")
+        if "secretMsg" in txt:
+            try:
+                m = [obj for obj in get_objects() if id(obj) == int(msg_id)][0]
+            except Exception as error:
+                return await send.edit(f"<b>❌ ᴇʀʀᴏʀ:</b> <code>{error}</code>")
+            user_or_me = [m.reply_to_message.from_user.id, m.from_user.id]
+            if message.from_user.id not in user_or_me:
+                return await send.edit(
+                    f"<b>❌ ᴘᴇsᴀɴ ɪɴɪ ʙᴜᴋᴀɴ ᴜɴᴛᴜᴋᴍᴜ <a href=tg://user?id={message.from_user.id}>{message.from_user.first_name} {message.from_user.last_name or ''}</a>"
+                )
+            else:
+                text = await client.send_message(
+                    message.chat.id,
+                    m.text.split(None, 1)[1],
+                    protect_content=True,
+                    reply_to_message_id=message.id,
+                )
+                await send.delete()
+                await asyncio.sleep(120)
+                await message.delete()
+                await text.delete()
+        elif "copyMsg" in txt:
+            try:
+                m = [obj for obj in get_objects() if id(obj) == int(msg_id)][0]
+            except Exception as error:
+                return await send.edit(f"<b>❌ ᴇʀʀᴏʀ:</b> <code>{error}</code>")
+            id_copy = int(m.text.split()[1].split("/")[-1])
+            if "t.me/c/" in m.text.split()[1]:
+                chat = int("-100" + str(m.text.split()[1].split("/")[-2]))
+            else:
+                chat = str(m.text.split()[1].split("/")[-2])
+            try:
+                get = await client.get_messages(chat, id_copy)
+                await get.copy(message.chat.id, reply_to_message_id=message.id)
+                await send.delete()
+            except Exception as error:
+                await send.edit(error)
