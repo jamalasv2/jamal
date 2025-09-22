f2ada05a04577a23cb18d810ec4bb091f1c7fb9c
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 13:46:04 +0700

    Add files via upload

---
+++ Jamal/core/helpers/inline.py
@@ -0,0 +1,180 @@
+from pykeyboard import InlineKeyboard
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import (InlineKeyboardButton, InlineQueryResultArticle,
+                            InputTextMessageContent)
+
+from PyroUbot import *
+from PyroUbot.config import *
+
+
+class Button:
+    def alive(get_id):
+        button = [
+            [
+                InlineKeyboardButton(
+                    text="ᴛᴜᴛᴜᴘ",
+                    callback_data="alv_cls",
+                )
+            ]
+        ]
+        return button
+
+    def button_add_expired(user_id):
+        buttons = InlineKeyboard(row_width=3)
+        keyboard = []
+        for X in range(1, 13):
+            keyboard.append(
+                InlineKeyboardButton(
+                    f"{X} ʙᴜʟᴀɴ",
+                    callback_data=f"success {user_id} {X}",
+                )
+            )
+        buttons.add(*keyboard)
+        buttons.row(
+            InlineKeyboardButton(
+                " ᴅᴀᴘᴀᴛᴋᴀɴ ᴘʀᴏꜰɪʟ ", callback_data=f"profil {user_id}"
+            )
+        )
+        buttons.row(
+            InlineKeyboardButton(
+                " ᴛᴏʟᴀᴋ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ", callback_data=f"failed {user_id}"
+            )
+        )
+        return buttons
+
+    def expired_button_bot():
+        button = [
+            [InlineKeyboardButton(" ʙᴇʟɪ ᴜsᴇʀʙᴏᴛ ", callback_data="bahan")]
+        ]
+        return button
+
+    def start(message):
+        button = [
+            [InlineKeyboardButton(" ʙᴇʟɪ ᴜsᴇʀʙᴏᴛ ", callback_data="bahan")],
+            [
+                InlineKeyboardButton(
+                    " sᴛᴀᴛᴜs ᴀᴋᴜɴ", callback_data="pler"
+                ),
+                InlineKeyboardButton(
+                    "ʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ ", callback_data="memek"
+                ),
+            ],
+            # [InlineKeyboardButton("🤩 ᴜsᴇʀʙᴏᴛ ɢʀᴀᴛɪs 🤩", callback_data="grts")],
+        ]
+        return button
+
+    def plus_minus(query, user_id):
+        button = [
+            [
+                InlineKeyboardButton(
+                    "-1",
+                    callback_data=f"kurang {query}",
+                ),
+                InlineKeyboardButton(
+                    "+1",
+                    callback_data=f"tambah {query}",
+                ),
+            ],
+            [InlineKeyboardButton(" ᴋᴏɴꜰɪʀᴍᴀsɪ ", callback_data="confirm")],
+            [InlineKeyboardButton("⬅️ ᴋᴇᴍʙᴀʟɪ", callback_data=f"home {user_id}")],
+        ]
+        return button
+
+    
+    def userbot(user_id, count):
+        button = [
+            [
+                InlineKeyboardButton(
+                    "📁 ʜᴀᴘᴜꜱ ᴅᴀʀɪ ᴅᴀᴛᴀʙᴀꜱᴇ 📁",
+                    callback_data=f"del_ubot {int(user_id)}",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "📲 ᴄᴇᴋ ɴᴏᴍᴏʀ 📲",
+                    callback_data=f"get_phone {int(count)}",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "⏳ ᴄᴇᴋ ᴋᴀᴅᴀʟᴜᴀʀꜱᴀ ⏳",
+                    callback_data=f"cek_masa_aktif {int(user_id)}",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "🔑 ᴄᴇᴋ ᴏᴛᴘ 🔑",
+                    callback_data=f"get_otp {int(count)}",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "🔐 ᴄᴇᴋ ᴠᴇʀɪꜰɪᴋᴀꜱɪ 2ʟ 🔐",
+                    callback_data=f"get_faktor {int(count)}",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "☠ ᴅᴇʟᴇᴛᴇ ᴀᴄᴄᴏᴜɴᴛ ☠", callback_data=f"ub_deak {int(count)}",
+                )
+            ],
+            [
+                InlineKeyboardButton("⬅️", callback_data=f"p_ub {int(count)}"),
+                InlineKeyboardButton("➡️", callback_data=f"n_ub {int(count)}"),
+            ],  
+        ]
+        return button
+
+    def deak(user_id, count):
+        button = [
+            [
+                InlineKeyboardButton(
+                    "⬅️ ᴋᴇᴍʙᴀʟɪ",
+                    callback_data=f"p_ub {int(count)}"
+                ),
+                InlineKeyboardButton(
+                    "sᴇᴛᴜJᴜɪ ✅", callback_data=f"deak_akun {int(count)}",
+                ),
+            ],
+        ]
+        return button
+
+class INLINE:
+    def QUERY(func):
+        async def wrapper(client, inline_query):
+            users = ubot._get_my_id
+            if inline_query.from_user.id not in users:
+                await client.answer_inline_query(
+                    inline_query.id,
+                    cache_time=1,
+                    results=[
+                        (
+                            InlineQueryResultArticle(
+                                title=f"ᴀɴᴅᴀ ʙᴇʟᴜᴍ ᴏʀᴅᴇʀ @{bot.me.username}",
+                                input_message_content=InputTextMessageContent(
+                                    f"sɪʟᴀʜᴋᴀɴ ᴏʀᴅᴇʀ ᴅɪ @{bot.me.username} ᴅᴜʟᴜ ʙɪᴀʀ ʙɪsᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ɪɴʟɪɴᴇ ɪɴɪ"
+                                ),
+                            )
+                        )
+                    ],
+                )
+            else:
+                await func(client, inline_query)
+
+        return wrapper
+
+    def DATA(func):
+        async def wrapper(client, callback_query):
+            users = ubot._get_my_id
+            if callback_query.from_user.id not in users:
+                await callback_query.answer(
+                    f"ᴍᴀᴋᴀɴʏᴀ ᴏʀᴅᴇʀ ᴜsᴇʀʙᴏᴛ @{bot.me.username} ᴅᴜʟᴜ ʙɪᴀʀ ʙɪsᴀ ᴋʟɪᴋ ᴛᴏᴍʙᴏʟ ɪɴɪ",
+                    True,
+                )
+            else:
+                try:
+                    await func(client, callback_query)
+                except MessageNotModified:
+                    await callback_query.answer("❌ ERROR")
+
+        return wrapper
---
+++ Jamal/core/helpers/text.py
@@ -0,0 +1,92 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from PyroUbot import OWNER_ID, bot, get_expired_date, ubot
+
+
+class MSG:
+    def DEAK(X):
+        return f"""
+<b>ᴘᴇᴍʙᴇʀɪᴛᴀʜᴜᴀɴ</b>
+<b>ᴀᴋᴜɴ:</b> <a href=tg://user?id={X.me.id}>{X.me.first_name} {X.me.last_name or ''}</a>
+<b>ɪᴅ:</b> <code>{X.me.id}</code>
+<b>ʀᴇᴀsᴏɴ:</b> <code>ᴅɪ ʜᴀᴘᴜs ᴅᴀʀɪ ᴛᴇʟᴇɢʀᴀᴍ</code>
+"""
+            
+    def EXPIRED_MSG_BOT(X):
+        return f"""
+<BLOCKQUOTE><b>ᴘᴇᴍʙᴇʀɪᴛᴀʜᴜᴀɴ</b>
+<b>ᴀᴋᴜɴ:</b> <a href=tg://user?id={X.me.id}>{X.me.first_name} {X.me.last_name or ''}</a>
+<b>ɪᴅ:</b> <code>{X.me.id}</code>
+<b>ᴇxᴘɪʀᴇᴅ: ᴛᴇʟᴀʜ ʜᴀʙɪs</b></BLOCKQUOTE>
+"""
+
+    
+    def START(message):
+        return f"""
+<BLOCKQUOTE><b>ʜᴀʟᴏ <a href=tg://user?id={message.from_user.id}>{message.from_user.first_name} {message.from_user.last_name or ''}</a>!
+
+<b>{bot.me.mention}</b> ᴀᴅᴀʟᴀʜ ᴜsᴇʀʙᴏᴛ ᴅᴇɴɢᴀɴ ʙᴇʙᴇʀᴀᴘᴀ ғɪᴛᴜʀ ʏᴀɴɢ ᴀᴋᴀɴ ᴍᴇᴍᴜᴅᴀʜᴋᴀɴ sᴇʙᴀɢɪᴀɴ ᴋᴇʙᴜᴛᴜʜᴀɴᴍᴜ ᴅɪ ᴛᴇʟᴇɢʀᴀᴍ sᴇᴘᴇʀᴛɪ: ɢʟᴏʙᴀʟ ʙʀᴏᴀᴅᴄᴀsᴛ, ᴏᴘᴇɴᴀɪ, ᴅᴏᴡɴʟᴏᴀᴅᴇʀ, ᴄᴀᴛᴀᴛᴀɴ, ᴅsʙ. ᴜsᴇʀʙᴏᴛ ɪɴɪ ʙɪsᴀ ᴋᴀʟɪᴀɴ ʙᴜᴀᴛ ᴅᴀɴ ᴋᴀʟɪᴀɴ ɢᴜɴᴀᴋᴀɴ ᴅᴇɴɢᴀɴ sᴀɴɢᴀᴛ ᴍᴜᴅᴀʜ.</BLOCKQUOTE>
+"""
+
+    def TEXT_PAYMENT(harga, total, bulan):
+        return f"""
+<BLOCKQUOTE><b>💬 sɪʟᴀʜᴋᴀɴ ᴍᴇʟᴀᴋᴜᴋᴀɴ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ</b>
+
+<b>🎟️ ʜᴀʀɢᴀ ᴘᴇʀʙᴜʟᴀɴ: {harga}.000</b>
+
+<b>💳 ᴍᴏᴛᴏᴅᴇ ᴘᴇᴍʙᴀʏᴀʀᴀɴ:</b>
+ <b>├──• ᴅᴀɴᴀ </b>
+ <b>├─• <code>‪085603220147</code></b>
+ <b>└──• <a href=https://link.dana.id/qr/c7g8ia1o>ᴋʟɪᴋ ᴅɪsɪɴɪ</a></b>
+
+<b>🔖 ᴛᴏᴛᴀʟ ʜᴀʀɢᴀ: ʀᴘ {total}.000</b>
+<b>🗓️ ᴛᴏᴛᴀʟ ʙᴜʟᴀɴ: {bulan}</b> 
+
+<b>✅ ᴋʟɪᴋ ᴛᴏᴍʙᴏʟ ᴋᴏɴꜰɪʀᴍᴀsɪ ᴜɴᴛᴜᴋ ᴋɪʀɪᴍ ʙᴜᴋᴛɪ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ᴀɴᴅᴀ</b></BLOCKQUOTE>
+"""
+
+    async def USERBOT(count):
+        return f"""
+<b>ᴜsᴇʀʙᴏᴛ ᴋᴇ</b> <code>{int(count) + 1}/{len(ubot._ubot)}</code>
+<b>ᴀᴋᴜɴ:</b> <a href=tg://user?id={ubot._ubot[int(count)].me.id}>{ubot._ubot[int(count)].me.first_name} {ubot._ubot[int(count)].me.last_name or ''}</a> 
+<b>ɪᴅ:</b> <code>{ubot._ubot[int(count)].me.id}</code>
+"""
+
+    def POLICY():
+        return """
+<BLOCKQUOTE>↪️ ᴋᴇʙɪᴊᴀᴋᴀɴ ᴘᴇɴɢᴇᴍʙᴀʟɪᴀɴ
+
+✅ sᴇᴛᴇʟᴀʜ ᴍᴇʟᴀᴋᴜᴋᴀɴ ᴘᴇᴍʙᴀʏᴀʀᴀɴ, ᴊɪᴋᴀ ᴀɴᴅᴀ ʙᴇʟᴜᴍ ᴍᴇᴍᴘᴇʀᴏʟᴇʜ/
+ᴍᴇɴᴇʀɪᴍᴀ ᴍᴀɴꜰᴀᴀᴛ ᴅᴀʀɪ ᴘᴇᴍʙᴇʟɪᴀɴ, 
+ᴀɴᴅᴀ ᴅᴀᴘᴀᴛ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ʜᴀᴋ ᴘᴇɴɢɢᴀɴᴛɪᴀɴ ᴅᴀʟᴀᴍ ᴡᴀᴋᴛᴜ 2 ʜᴀʀɪ sᴇᴛᴇʟᴀʜ ᴘᴇᴍʙᴇʟɪᴀɴ. ɴᴀᴍᴜɴ, ᴊɪᴋᴀ 
+ᴀɴᴅᴀ ᴛᴇʟᴀʜ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ/ᴍᴇɴᴇʀɪᴍᴀ sᴀʟᴀʜ sᴀᴛᴜ ᴍᴀɴꜰᴀᴀᴛ ᴅᴀʀɪ 
+ᴘᴇᴍʙᴇʟɪᴀɴ, ᴛᴇʀᴍᴀsᴜᴋ ᴀᴋsᴇs ᴋᴇ ꜰɪᴛᴜʀ ᴘᴇᴍʙᴜᴀᴛᴀɴ ᴜsᴇʀʙᴏᴛ, ᴍᴀᴋᴀ 
+ᴀɴᴅᴀ ᴛɪᴅᴀᴋ ʟᴀɢɪ ʙᴇʀʜᴀᴋ ᴀᴛᴀs ᴘᴇɴɢᴇᴍʙᴀʟɪᴀɴ ᴅᴀɴᴀ.
+
+🆘 ᴅᴜᴋᴜɴɢᴀɴ
+ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴀᴘᴀᴛᴋᴀɴ ᴅᴜᴋᴜɴɢᴀɴ, ᴀɴᴅᴀ ᴅᴀᴘᴀᴛ:
+• ᴍᴇɴɢʜᴜʙᴜɴɢɪ ᴀᴅᴍɪɴ : roronoajamalsv2.t.me
+• sᴜᴘᴘᴏʀᴛ jamalsupportt.t.me ᴅɪ ᴛᴇʟᴇɢʀᴀᴍ
+
+👉🏻 ᴛᴇᴋᴀɴ ᴛᴏᴍʙᴏʟ ʟᴀɴᴊᴜᴛᴋᴀɴ ᴜɴᴛᴜᴋ ᴍᴇɴʏᴀᴛᴀᴋᴀɴ ʙᴀʜᴡᴀ ᴀɴᴅᴀ ᴛᴇʟᴀʜ 
+ᴍᴇᴍʙᴀᴄᴀ ᴅᴀɴ ᴍᴇɴᴇʀɪᴍᴀ ᴋᴇᴛᴇɴᴛᴜᴀɴ ɪɴɪ ᴅᴀɴ ᴍᴇʟᴀɴᴊᴜᴛᴋᴀɴ 
+ᴘᴇᴍʙᴇʟɪᴀɴ. ᴊɪᴋᴀ ᴛɪᴅᴀᴋ, ᴛᴇᴋᴀɴ ᴛᴏᴍʙᴏʟ ʙᴀᴛᴀʟᴋᴀɴ.</BLOCKQUOTE>
+"""
+
+
+async def sending_user(user_id):
+        await bot.send_message(
+            user_id,
+            "💬 sɪʟᴀʜᴋᴀɴ ʙᴜᴀᴛ ᴜʟᴀɴɢ ᴜsᴇʀʙᴏᴛ ᴀɴᴅᴀ",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "🔥 ʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ 🔥",
+                            callback_data="bahan",
+                        )
+                    ],
+                ]
+            ),
+            disable_web_page_preview=True,
+        )
