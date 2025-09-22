549ba82c197b4eadbfdfdb76f659a45602e6a2e6
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 15:37:30 +0700

    Update userbot.py

--- Jamal/database/userbot.py
+++ Jamal/database/userbot.py
@@ -2,28 +2,32 @@ from Jamal.database import mongodb
 
 ubotdb = mongodb.ubot
 
+
 async def add_ubot(user_id, api_id, api_hash, session_string):
     return await ubotdb.update_one(
-        {"user_id": int(user_id)},
-        {"$set": {
-            "user_id": int(user_id),
-            "api_id": int(api_id),
-            "api_hash": api_hash,
-            "session_string": session_string,
-        }},
+        {"user_id": user_id},
+        {
+            "$set": {
+                "api_id": api_id,
+                "api_hash": api_hash,
+                "session_string": session_string,
+            }
+        },
         upsert=True,
     )
 
+
 async def remove_ubot(user_id):
-    return await ubotdb.delete_one({"user_id": int(user_id)})
+    return await ubotdb.delete_one({"user_id": user_id})
+
 
 async def get_userbots():
     data = []
-    async for ubot in ubotdb.find({"user_id": {"$exists": True}}):
+    async for ubot in ubotdb.find({"user_id": {"$exists": 1}}):
         data.append(
             dict(
-                user_id=int(ubot["user_id"]),
-                api_id=int(ubot["api_id"]),
+                name=str(ubot["user_id"]),
+                api_id=ubot["api_id"],
                 api_hash=ubot["api_hash"],
                 session_string=ubot["session_string"],
             )
