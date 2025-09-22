1624fac5ced79aa0f5c1f9e9018bc3f4e8eb2a8a
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 23:12:24 +0700

    Update payment.py

--- Jamal/modules/payment.py
+++ Jamal/modules/payment.py
@@ -5,7 +5,7 @@ from dateutil.relativedelta import relativedelta
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pytz import timezone
 
-from PyroUbot import *
+from Jamal import *
 
 CONFIRM_PAYMENT = []
 
