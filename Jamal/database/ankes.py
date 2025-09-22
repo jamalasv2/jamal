15f8830e849195ba9a0f5d95ca59de6ae40557f0
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 14:06:15 +0700

    Add files via upload

---
+++ Jamal/database/ankes.py
@@ -0,0 +1,23 @@
+from PyroUbot.core.database import mongodb
+
+ankesdb = mongodb.ankes
+
+async def get_ankes(user_id):
+    chat = await ankesdb.find_one({"chat": user_id})
+    if not chat:
+        return []
+    return chat["list"]
+
+
+async def add_ankes(user_id, chat_id):
+    list = await get_ankes(user_id)
+    list.append(chat_id)
+    await ankesdb.update_one({"chat": user_id}, {"$set": {"list": list}}, upsert=True)
+    return True
+
+
+async def remove_ankes(user_id, chat_id):
+    list = await get_ankes(user_id)
+    list.remove(chat_id)
+    await ankesdb.update_one({"chat": user_id}, {"$set": {"list": list}}, upsert=True)
+    return True
