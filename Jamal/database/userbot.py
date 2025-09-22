d059395da8d1e24e480fe27e5ec4d253f42d8958
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 15:10:48 +0700

    Update userbot.py

--- Jamal/database/userbot.py
+++ Jamal/database/userbot.py
@@ -4,9 +4,10 @@ ubotdb = mongodb.ubot
 
 async def add_ubot(user_id, api_id, api_hash, session_string):
     return await ubotdb.update_one(
-        {"user_id": user_id},
+        {"user_id": int(user_id)},
         {"$set": {
-            "api_id": api_id,
+            "user_id": int(user_id),
+            "api_id": int(api_id),
             "api_hash": api_hash,
             "session_string": session_string,
         }},
@@ -14,11 +15,11 @@ async def add_ubot(user_id, api_id, api_hash, session_string):
     )
 
 async def remove_ubot(user_id):
-    return await ubotdb.delete_one({"user_id": user_id})
+    return await ubotdb.delete_one({"user_id": int(user_id)})
 
 async def get_userbots():
     data = []
-    async for ubot in ubotdb.find({"user_id": {"$exists": 1}}):
+    async for ubot in ubotdb.find({"user_id": {"$exists": True}}):
         data.append(
             dict(
                 user_id=int(ubot["user_id"]),
