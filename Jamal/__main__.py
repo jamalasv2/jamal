6e9d22e12911224939bb16e29b6bca50a3ade419
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 15:05:04 +0700

    Update __main__.py

--- Jamal/__main__.py
+++ Jamal/__main__.py
@@ -1,35 +1,44 @@
 import asyncio
 import signal
-
 import tornado.ioloop
 import tornado.platform.asyncio
-
 from pyrogram import idle
 from pyrogram.errors import UserDeactivated
+from pyrogram.session import StringSession
 
 from Jamal import *
-from Jamal.database import *
+from Jamal.database.ubot import get_userbots, remove_ubot
+
+async def shutdown(sig, loop):
+    print(f"[INFO] Received exit signal {sig.name}...")
+    tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+    [task.cancel() for task in tasks]
+    await asyncio.gather(*tasks, return_exceptions=True)
+    loop.stop()
 
 async def main():
     await bot.start()
     for _ubot in await get_userbots():
-        ubot_ = Ubot(**_ubot)
+        ubot_ = Ubot(
+            session_name=StringSession(_ubot["session_string"]),
+            api_id=_ubot["api_id"],
+            api_hash=_ubot["api_hash"],
+            name=str(_ubot["user_id"]),
+        )
         try:
             await asyncio.wait_for(ubot_.start(), timeout=10)
             await ubot_.join_chat("newhiganbana")
         except asyncio.TimeoutError:
-            print(f"[𝗜𝗡𝗙𝗢]: {int(_ubot['name'])} 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
+            print(f"[INFO]: {_ubot['user_id']} TIDAK MERESPON")
         except Exception as e:
-            await remove_ubot(int(_ubot["name"]))
-            await rem_expired_date(int(_ubot["name"]))
-            print(f"[𝗜𝗡𝗙𝗢]: {int(_ubot['name'])}\n{e}")
+            await remove_ubot(int(_ubot["user_id"]))
+            print(f"[INFO]: {_ubot['user_id']}\n{e}")
         except UserDeactivated:
-            await remove_ubot(int(_ubot["name"]))
-            await rem_expired_date(int(_ubot["name"]))
-            await remove_all_vars(int(_ubot["name"]))
-            await remove_from_vars(bot.me.id, "PREM_USERS", int(_ubot["name"]))
-            print(f"[ INFO ]: {int(_ubot['name'])} akun terhapus dibersihkan")
+            await remove_ubot(int(_ubot["user_id"]))
+            print(f"[INFO]: {_ubot['user_id']} akun terhapus dibersihkan")
+
     await asyncio.gather(loadPlugins(), installPeer(), expiredUserbots(), idle())
+
     stop_event = asyncio.Event()
     loop = asyncio.get_running_loop()
     for s in (signal.SIGINT, signal.SIGTERM):
