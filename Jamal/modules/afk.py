8778a498b2df94d0063c199e6bb9b902d33709f2
jamalasv2 <jamalasbanget@gmail.com>
2025-09-24 01:28:34 +0700

    Update afk.py

--- Jamal/modules/afk.py
+++ Jamal/modules/afk.py
@@ -40,6 +40,6 @@ async def _(client, message):
     if vars:
         afk_time = vars.get("time")
         afk_runtime = await get_time(time() - afk_time)
-        teks = bhs("filters_unafk").format(em.afk, em.menunggu, em.keterangan, afk_runtime)
+        teks = bhs("filters_unafk").format(em.afk, em.menunggu, afk_runtime)
         await message.reply(teks)
         return await remove_vars(client.me.id, "AFK")
