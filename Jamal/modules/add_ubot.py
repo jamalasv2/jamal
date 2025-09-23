c5661c84171f200fcf26c75d03f260c96ed0a05c
jamalasv2 <jamalasbanget@gmail.com>
2025-09-23 12:45:57 +0700

    Update add_ubot.py

--- Jamal/modules/add_ubot.py
+++ Jamal/modules/add_ubot.py
@@ -236,7 +236,7 @@ async def _(client, callback_query):
     buttons = [
             [InlineKeyboardButton(bhs("text_back"), callback_data=f"home {user_id}")],
         ]
-    text_done = bhs("text_activated").format(f"<a href=tg://user?id={new_client.me.id}>{new_client.me.first_name} {new_client.me.last_name or ''}</a>", new_client.me.id, ''.join(sh), xx), 
+    text_done = bhs("text_activated").format(f"<a href=tg://user?id={new_client.me.id}>{new_client.me.first_name} {new_client.me.last_name or ''}</a>", new_client.me.id, ''.join(SH), xx), 
     await bot_msg.edit(text_done, disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(buttons))
     await install_my_peer(new_client)
