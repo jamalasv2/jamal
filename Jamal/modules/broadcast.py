3fa0e9540da33921ed8e4d3cd03dbde5f9c12fb8
jamalasv2 <jamalasbanget@gmail.com>
2025-09-27 00:55:26 +0700

    Update broadcast.py

--- Jamal/modules/broadcast.py
+++ Jamal/modules/broadcast.py
@@ -124,3 +124,158 @@ async def _(client, message):
     await msg.delete()
     return await message.reply(stat)
             
+
+@PY.UBOT("addbl", sudo=True)
+@ubot.on_message(filters.command(["addbl"], "C") & filters.user(DEVS))
+async def _(client, message):
+    em = get_emo(client)
+    per = message.command[1] if len(message.command) > 1 else message.chat.id
+    tm = await message.reply(bhs("text_proses").format(em.proses))
+    if message.chat.type == (ChatType.GROUP, ChatType.SUPERGROUP):
+        blacklist = await get_chat(client.me.id)
+        try:
+            chat = await client.get_chat(per)
+        except Exception as error:
+            return await tm.edit(bhs("text_error").format(em.gagal, error))
+
+        if chat.id in blacklist:
+            return await tm.edit(bhs("blacklist_exist").format(em.gagal, 'group'))
+
+        try:
+            await add_chat(client.me.id, chat.id)
+            await tm.edit(bhs("blacklist_sukses").format(em.berhasil, titit.title))
+            await asyncio.sleep(8)
+            await message.delete()
+            return await tm.delete()
+
+        except Exception as error:
+            return tm.edit(bhs("text_error").format(em.gagal, error))
+
+    if message.chat.type == ChatType.PRIVATE:
+        blacklist = await get_list_from_vars(client.me.id, "BL_UCAST", "DB_UCAST")
+        try:
+            user = await client.get_users(per)
+        except Exception as error:
+            return await tm.edit(bhs("text_error").format(em.gagal, error))
+
+        if user.id in blacklist:
+            return await tm.edit(bhs("blacklist_exist").format(em.gagal, 'user'))
+
+        try:
+            await add_to_vars(client.me.id, "BL_UCAST", user.id, "DB_UCAST")
+            await tm.edit(bhs("blacklist_sukses").format(em.berhasil, user.id))
+            await asyncio.sleep(8)
+            await message.delete()
+            return await tm.delete()
+
+        except Exception as error:
+            return await tm.edit(bhs("blacklist_failed").format(em.gagal))
+
+
+@PY.UBOT("unbl", sudo=True)
+@ubot.on_message(filters.command(["unbl"], "C") & filters.user(DEVS))
+async def _(client, message):
+    em = get_emo(client)
+    per = message.command[1] if len(message.command) > 1 else message.chat.id
+    tm = await message.reply(bhs("text_proses").format(em.proses))
+    if message.chat.type == (ChatType.GROUP, ChatType.SUPERGROUP):
+        blacklist = await get_chat(client.me.id)
+        try:
+            chat = await client.get_chat(per)
+        except Exception as error:
+            return await tm.edit(bhs("text_error").format(em.gagal, error))
+
+        if chat.id not in blacklist:
+            return await tm.edit("
+        
+    if per not in blacklist:
+        return await Tm.edit(
+            f"<BLOCKQUOTE>{ggl} grup {titit.title} tidak ada di daftar hitam broadcast</BLOCKQUOTE>"
+        )
+    try:
+        await remove_chat(client.me.id, titit.id)
+        return await Tm.edit(
+            f"<BLOCKQUOTE><b>{brhsl} dihapus dari daftar hitam broadcast\n{gc} grup: {titit.title}</b></BLOCKQUOTE>"
+        )
+    except Exception:
+        await remove_chat(client.me.id, titit.id)
+        return await Tm.edit(
+            f"<BLOCKQUOTE><b>{brhsl} dihapus dari daftar hitam broadcast</b></BLOCKQUOTE>"
+        )
+    except Exception as error:
+        return await Tm.edit(f"<blockquote>{ggl} ERROR\n{error}</blockquote>")
+
+
+@PY.UBOT("rallbl", sudo=True)
+async def _(client, message):
+    prs = await EMO.PROSES(client)
+    brhsl = await EMO.BERHASIL(client)
+    ggl = await EMO.GAGAL(client)
+    msg = await message.reply(f"{prs}<b>ᴍᴇᴍᴘʀᴏsᴇs..</b>")
+    get_bls = await get_chat(client.me.id)
+    if len(get_bls) == 0:
+        return await msg.edit(f"{ggl} daftar hitam broadcast anda kosong")
+    for X in get_bls:
+        await remove_chat(client.me.id, X)
+    await msg.edit(f"<BLOCKQUOTE>{brhsl} daftar hitam broadcast berhasil dihapus</BLOCKQUOTE>")
+
+
+@PY.UBOT("listbl", sudo=True)
+async def _(client, message):
+    prs = await EMO.PROSES(client)
+    Tm = await message.reply(f"<b>ᴍᴇᴍᴘʀᴏsᴇs..</b>")
+    msg = f"<b>❏ total grup yang berada didaftar hitam: {len(await get_chat(client.me.id))}</b>\n\n"
+    for X in await get_chat(client.me.id):
+        try:
+            get = await client.get_chat(X)
+            msg += f"<b>├ {get.title}</b> | <code>{get.id}</code>\n"
+        except:
+            msg += f"├<code> {X}</code>\n"
+    await Tm.delete()
+    await message.reply(f"<BLOCKQUOTE>{msg}</BLOCKQUOTE>")
+
+
+@PY.UBOT("delucast")
+@PY.PRIVATE
+async def _(client, message):
+    brhsl = EMO.BERHASIL(client)
+    ggl = EMO.GAGAL(client)
+    tion = EMO.MENTION(client)
+    try:
+        if not get_arg(message):
+            chat_id = message.chat.id
+        else:
+            chat_id = int(message.command[1])
+    except Exception as e:
+        return await message.reply(f"{e}")
+    blacklist = await get_list_from_vars(client.me.id, "BLUCAST", "DB_UCAST")
+    if chat_id not in blacklist:
+        return await message.reply(
+            f"{ggl} pengguna tidak berada dalam daftar hitam"
+        )
+    else:
+        await remove_from_vars(client.me.id, "BLUCAST", chat_id, "DB_UCAST")
+        return await message.reply(
+            f"{brhsl}pengguna berhasil dihapus dari daftar hitam ucast"
+        )
+
+
+@PY.UBOT("listucast")
+async def _(client, message):
+    msg = await message.reply("memproses")
+    ucast = await get_list_from_vars(client.me.id, "BLUCAST", "DB_UCAST")
+    Tm = f"<b>❏ daftar hitam ucast</b>\n\n"
+    if not ucast:
+        return await msg.edit(f"daftar hitam kosong")
+
+    for user_id in ucast:
+        try:
+            get = await client.get_users(user_id)
+            Tm += f"├ {get.first_name} {get.last_name or ''} | <code>{get.id}</code>\n"
+        except:
+            Tm += f"├ <code>{user_id}</code>\n"
+    await msg.delete()
+    return await message.reply(f"<BLOCKQUOTE>{Tm}</BLOCKQUOTE>")
+
+
+
