e667b3ced354005ea5d4cfcf2adee32806156566
jamalasv2 <jamalasbanget@gmail.com>
2025-09-29 02:02:06 +0700

    Update global.py

--- Jamal/modules/global.py
+++ Jamal/modules/global.py
@@ -1,45 +1,26 @@
 import asyncio
 
 from pyrogram import *
-from pyrogram.enums import *
+from pyrogram.enums import ChatType
 from pyrogram.errors import *
 from pyrogram.types import *
 
 from Jamal import *
+from langs import bhs, get_bhs
 
 
 __MODULE__ = "global"
-__HELP__ = """
-<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ɢʟᴏʙᴀʟ 』</b>
-
-<b>❏ ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}gban</ᴄᴏᴅᴇ> [ᴜsᴇʀ_ɪᴅ/ᴜsᴇʀɴᴀᴍᴇ/ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ]
- <i> ᴜɴᴛᴜᴋ ʙᴀɴɴᴇᴅ ᴜsᴇʀ ᴅᴀʀɪ sᴇᴍᴜᴀ ɢʀᴏᴜᴘ ᴄʜᴀᴛ</i>
-
-<b>❏ ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}ungban</code> [ᴜsᴇʀ_ɪᴅ/ᴜsᴇʀɴᴀᴍᴇ/ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ]
- <i> ᴜɴᴛᴜᴋ ᴜɴʙᴀɴɴᴇᴅ ᴜsᴇʀ ᴅᴀʀɪ sᴇᴍᴜᴀ ɢʀᴏᴜᴘ ᴄʜᴀᴛ</i>
- 
-<b>❏ ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}gmute</code> [ᴜsᴇʀ_ɪᴅ/ᴜsᴇʀɴᴀᴍᴇ/ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ]
- <i>ᴜɴᴛᴜᴋ ᴍᴜᴛᴇ ᴜsᴇʀ ᴅᴀʀɪ sᴇᴍᴜᴀ ɢʀᴏᴜᴘ ᴄʜᴀᴛ</i>
-
-<b>❏ ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}ungmute</code> [ᴜsᴇʀ_ɪᴅ/ᴜsᴇʀɴᴀᴍᴇ/ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ]
- <i>ᴜɴᴛᴜᴋ ᴜɴᴍᴜᴛᴇ ᴜsᴇʀ ᴅᴀʀɪ sᴇᴍᴜᴀ ɢʀᴏᴜᴘ ᴄʜᴀᴛ</i>
-"""
+__HELP__ = get_bhs("global_cmd")
 
 
 @PY.UBOT("gban", sudo=True)
-@PY.TOP_CMD
-@ubot.on_message(filters.command(["gban"], "C") & filters.user(DEVS))
+@ubot.on_message(filters.command(["gban"], "C") & filters.user(SUDO))
 async def _(client, message):
-    prs = await EMO.PROSES(client)
-    brhsl = await EMO.BERHASIL(client)
-    ggl = await EMO.GAGAL(client)
-    gc = await EMO.BL_GROUP(client)
-    ktrg = await EMO.BL_KETERANGAN(client)
-    xtion = await EMO.MENTION(client)
+    em = await get_emo(client)
     user_id = await extract_user(message)
-    Tm = await message.reply(f"<b>{prs}ᴍᴇᴍᴘʀᴏsᴇs</b>")
+    Tm = await message.reply(bhs("text_proses").format(em.proses))
     if not user_id:
-        return await Tm.edit(f"<b>{ggl}ᴜsᴇʀ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
+        return await Tm.edit(bhs("global_nfound").format(em.gagal, 'banned'))
     try:
         user = await client.get_users(user_id)
     except Exception as error:
@@ -48,8 +29,8 @@ async def _(client, message):
     failed = 0
     global_id = await get_global_id(client, "global")
     for dialog in global_id:
-        if user.id == DEVS:
-            return await Tm.edit(f"<b>{ggl}ᴀɴᴅᴀ ᴛɪᴅᴀᴋ ʙɪsᴀ ɢʙᴀɴ ᴅɪᴀ ᴋᴀʀᴇɴᴀ ᴅɪᴀ ᴘᴇᴍʙᴜᴀᴛ sᴀʏᴀ</b>")
+        if user.id in DEVS or user.id in SUDO:
+            return await Tm.edit(bhs("global_dev").format(em.gagal, 'banned'))
         try:
             await client.ban_chat_member(dialog, user.id)
             done += 1
@@ -57,24 +38,18 @@ async def _(client, message):
         except Exception:
             failed += 1
             await asyncio.sleep(0.1)
-    await message.reply(f"<b>{brhsl}ᴘᴇʀɪɴᴛᴀʜ ʙᴇʀʜᴀsɪʟ!</b>\n\n{gc}ᴊᴜᴍʟᴀʜ: {done} ᴄʜᴀᴛ\n{ggl}ɢᴀɢᴀʟ: {failed} ᴄʜᴀᴛ\n{xtion}ᴜsᴇʀ: {user.mention}\n{ktrg}ᴋᴇᴛ: ɢʟᴏʙᴀʟ ʙᴀɴɴᴇᴅ</b>")
-    return await Tm.delete()
+	await Tm.delete()
+    return await message.reply(bhs("global_sukses").format(em.berhasil, 'banned', em.mention, user.mention, em.total, done))
 
 
 @PY.UBOT("ungban", sudo=True)
-@PY.TOP_CMD
-@ubot.on_message(filters.command(["ungban"], "C") & filters.user(DEVS))
+@ubot.on_message(filters.command(["ungban"], "C") & filters.user(SUDO))
 async def _(client, message):
-    prs = await EMO.PROSES(client)
-    brhsl = await EMO.BERHASIL(client)
-    ggl = await EMO.GAGAL(client)
-    gc = await EMO.BL_GROUP(client)
-    ktrg = await EMO.BL_KETERANGAN(client)
-    xtion = await EMO.MENTION(client)
+    em = await get_emo(client)
     user_id = await extract_user(message)
-    Tm = await message.reply(f"<b>{prs}ᴍᴇᴍᴘʀᴏsᴇs..</b>")
+    Tm = await message.reply(bhs("text_proses").format(em.proses))
     if not user_id:
-        return await Tm.edit("f<b>{ggl}ᴜsᴇʀ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
+        return await Tm.edit(bhs("global_nfound").format(em.gagal, 'unbanned'))
     try:
         user = await client.get_users(user_id)
     except Exception as error:
@@ -90,34 +65,28 @@ async def _(client, message):
         except Exception:
             failed += 1
             await asyncio.sleep(0.1)
-    await message.reply(f"<b>{brhsl}ᴘᴇʀɪɴᴛᴀʜ ʙᴇʀʜᴀsɪʟ\n\n{gc}ᴊᴜᴍʟᴀʜ: {done} ᴄʜᴀᴛ\n{ggl}ɢᴀɢᴀʟ: {failed} ᴄʜᴀᴛ\n{xtion}ᴜsᴇʀ: {user.mention}\n{ktrg}ᴋᴇᴛ: ɢʟᴏʙᴀʟ ᴜɴʙᴀɴɴᴇᴅ</b>")
-    return await Tm.delete()
+	await Tm.delete()
+    return await message.reply(bhs("global_sukses").format(em.berhasil, 'unbanned', em.mention, user.mention, em.total, done))
 
 
 @PY.UBOT("gmute", sudo=True)
-@PY.TOP_CMD
-@ubot.on_message(filters.command(["gmute"], "C") & filters.user(DEVS))
+@ubot.on_message(filters.command(["gmute"], "C") & filters.user(SUDO))
 async def _(client, message):
-	prs = await EMO.PROSES(client)
-	brhsl = await EMO.BERHASIL(client)
-	ggl = await EMO.GAGAL(client)
-	xtion = await EMO.MENTION(client)
-	gc = await EMO.BL_GROUP(client)
-	ktrg = await EMO.BL_KETERANGAN(client)
+	em = await get_emo(client)
 	user_id = await extract_user(message)
-	Tm = await message.reply(f"<b>{prs}ᴍᴇᴍᴘʀᴏsᴇs</b>")
+	Tm = await message.reply(bhs("text_proses").format(em.proses))
 	if not user_id:
-		return await Tm.edit(f"<b>{ggl}ᴜsᴇʀ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
+		return await Tm.edit(bhs("global_nfound").format(em.gagal, 'mute'))
 	try:
 		user = await client.get_users(user_id)
 	except Exception as error:
 		return await Tm.edit(error)
 	done = 0
 	failed = 0
-	global_id = await get_global_id(client, "global")
+	global_id = await get_global_id(client, "group")
 	for dialog in global_id:
-		if user.id == DEVS:
-			return await Tm.edit(f"<b>{ggl}ᴋᴀᴜ ɢᴀᴋ ʙɪsᴀ ɢᴍᴜᴛᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ ʙᴏᴛᴍᴜ</b>")
+		if user.id in DEVS or user.id in SUDO:
+			return await Tm.edit(bhs("global_dev").format(em.gagal, 'mute'))
 		try:
 			await client.restrict_chat_member(dialog, user.id, ChatPermissions())
 			done += 1
@@ -125,31 +94,25 @@ async def _(client, message):
 		except Exception:
 			failed += 1
 			await asyncio.sleep(0.1)
-	await message.reply(f"<b>{brhsl}ᴘᴇʀɪɴᴛᴀʜ ʙᴇʀʜᴀsɪʟ!\n\n{gc}ᴊᴜᴍʟᴀʜ: {done} ᴄʜᴀᴛ\n{ggl}ɢᴀɢᴀʟ: {failed} ᴄʜᴀᴛ\n{xtion}ᴜsᴇʀ: {user.mention}\n{ktrg}ᴋᴇᴛ: ɢʟᴏʙᴀʟ ᴍᴜᴛᴇ</b>")
-	return await Tm.delete()
+	await Tm.delete()
+	return await message.reply(bhs("global_sukses").format(em.berhasil, 'mute', em.mention, user.mention, em.total, done))
 
 
 @PY.UBOT("ungmute", sudo=True)
-@PY.TOP_CMD
-@ubot.on_message(filters.command(["ungmute"], "C") & filters.user(DEVS))
+@ubot.on_message(filters.command(["ungmute"], "C") & filters.user(SUDO))
 async def _(client, message):
-	prs = await EMO.PROSES(client)
-	brhsl = await EMO.BERHASIL(client)
-	ggl = await EMO.GAGAL(client)
-	gc = await EMO.BL_GROUP(client)
-	ktrg = await EMO.BL_KETERANGAN(client)
-	xtion = await EMO.MENTION(client)
+	em = await get_emo(client)
 	user_id = await extract_user(message)
-	Tm = await message.reply(f"<b>{prs}ᴍᴇᴍᴘʀᴏsᴇs</b>")
+	Tm = await message.reply(bhs("text_proses").format(em.proses))
 	if not user_id:
-		return await Tm.edit(f"<b>{ggl}ᴜsᴇʀ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ")
+		return await Tm.edit(bhs("global_nfound").format(em.gagal, 'unmute'))
 	try:
 		user = await client.get_users(user_id)
 	except Exception as error:
 		return await Tm.edit(error)
 	done = 0
 	failed = 0
-	global_id = await get_global_id(client, "global")
+	global_id = await get_global_id(client, "group")
 	for dialog in global_id:
 		try:
 			await client.unban_chat_member(dialog, user.id)
@@ -158,7 +121,7 @@ async def _(client, message):
 		except Exception:
 			failed += 1
 			await asyncio.sleep(0.1)
-	await message.reply(f"<b>{brhsl}ᴘᴇʀɪɴᴛᴀʜ ʙᴇʀʜᴀsɪʟ!\n\n{gc}ᴊᴜᴍʟᴀʜ: {done} ᴄʜᴀᴛ\n{ggl}ɢᴀɢᴀʟ: {failed} ᴄʜᴀᴛ\n{xtion}ᴜsᴇʀ: {user.mention}\n{ktrg}ᴋᴇᴛ: ɢʟᴏʙᴀʟ ᴜɴᴍᴜᴛᴇ</b>")
-	return await Tm.delete()
-			
+	await Tm.delete()
+	return await message.reply(bhs("global_sukses").format(em.berhasil, 'unmute', em.mention, user.mention, em.total, done))
+		
 
