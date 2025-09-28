e06b710570d29a40c28d0f4718cd7848b9bb54a5
jamalasv2 <jamalasbanget@gmail.com>
2025-09-29 00:08:21 +0700

    Update __init__.py

--- Jamal/__init__.py
+++ Jamal/__init__.py
@@ -76,7 +76,7 @@ class Ubot(Client):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs, device_model="higanbana-userbot")
-        self.group_call = GroupCallFactory(self).get_file_group_call()
+        self.group_call = GroupCallFactory(self).get_group_call()
     
     def on_message(self, filters=None, group=-1):
         def decorator(func):
