b8595523f333308c7fb696c924bcd90845e5e334
jamalasv2 <jamalasbanget@gmail.com>
2025-10-01 18:23:32 +0700

    Update ping.py

--- Jamal/modules/ping.py
+++ Jamal/modules/ping.py
@@ -6,13 +6,13 @@ from pyrogram.raw.functions import Ping
 
 from Jamal.core.helpers._client import PY
 from Jamal.helpers.class_emoji import get_emo
-from Jamal import ubot
+from Jamal import ubot, bot
 from Jamal.config import SUDO
 from Jamal bhs, get_bhs
 
 
 @PY.UBOT("ping|p", sudo=True)
-@ubot.on_message(filters.command(["ping"], "C") & filters.user(DEVS))
+@ubot.on_message(filters.command(["ping"], "C") & filters.user(SUDO))
 async def _(client, message):
     em = get_emo(client)
     start = datetime.now()
@@ -20,10 +20,6 @@ async def _(client, message):
     end = datetime.now()
     delta_ping = (end - start).microseconds / 1000
     delta_ping_formatted = round(delta_ping, 2)
-    _ping = f"""
-<BLOCKQUOTE><b>{ping}ᴘᴏɴɢ ~ </b> <code>{delta_ping} ms</code>
-<b>{tion}ᴜsᴇʀ ~ </b> <b>{client.me.mention}</b></BLOCKQUOTE>
-"""
-    return await message.reply(_ping)
+    return await message.reply(bhs("ping_text").format(em.ping, delta_ping, em.account, client.me.mention, em.ubot, bot.me.mention))
 
 
