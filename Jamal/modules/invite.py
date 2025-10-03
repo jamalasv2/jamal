9298ac8d1fcbdb9bd15c32002a5e84655524a16e
jamalasv2 <jamalasbanget@gmail.com>
2025-10-03 20:15:50 +0700

    Update invite.py

--- Jamal/modules/invite.py
+++ Jamal/modules/invite.py
@@ -25,9 +25,10 @@ async def _(client, message):
     user_list = user_to_add.split(" ")
     try:
         await client.add_chat_members(message.chat.id, user_list, forward_limit=100)
+        await asyncio.sleep(1)
+        await mg.edit(bhs("invite_sukses").format(em.berhasil, len(user_list), message.chat.title))
     except Exception as e:
         return await mg.edit(bhs("text_error").format(em.gagal, e)
-    await mg.edit(bhs("invite_sukses").format(em.berhasil, len(user_list), message.chat.title))
 
 
 invite_id = []
