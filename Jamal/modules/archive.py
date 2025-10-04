e004668232c00ada0acd9a0a801f62c2e9b775e3
jamalasv2 <jamalasbanget@gmail.com>
2025-10-05 02:11:08 +0700

    Update archive.py

--- Jamal/modules/archive.py
+++ Jamal/modules/archive.py
@@ -4,7 +4,7 @@ from pyrogram.errors.exceptions import FloodWait
 from pyrogram.enums import ChatType
 
 from Jamal import *
-from Jamal.core.helpers import PY, get_global_id, extract_type_and_msg
+from Jamal.core.helpers import PY, get_global_id
 from langs import bhs, get_bhs
 
 __MODULE__ = "archive"
@@ -43,65 +43,17 @@ async def _(client, message):
     if len(message.command) < 2:
         return await msg.edit(bhs("arsip_ggl").format(em.gagal))
 
-    command, query = message.command[:2]
-    chat = await get_global_id(client, query)
     done = 0
+    command, query = message.command[:2]
+    chats = await get_global_id(client, query)
 
-    if query.lower() == "all":
+    if query not in ["all", "bot", "channel", "personal"]:
+        return await msg.edit(bhs("arsip_ggl").format(em.gagal))
+
+    for chat_id in chats:
         try:
-            await client.unarchive_chats(chat)
+            await client.unarchive_chats(chat_id)
             await msg.delete()
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'all'))
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await client.unarchive_chats(chat)
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'all'))
-        except Exception as e:
-            return await msg.edit(bhs("text_error").format(em.gagal, e))
-    elif query.lower() == "gc":
-        try:
-            await client.unarchive_chats(chat)
-            await msg.delete()
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'group'))
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await client.unarchive_chats(chat)
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'group'))
-        except Exception as e:
-            return await msg.edit(bhs("text_error").format(em.gagal, e))
-    elif query.lower() == "pc":
-        try:
-            await client.unarchive_chats(chat)
-            done += 1
-            await msg.delete()
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'personal'))
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await client.unarchive_chats(chat)
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'personal'))
-        except Exception as e:
-            return await msg.edit(bhs("text_error").format(em.gagal, e))
-    elif query.lower() == "bot":
-        try:
-            await client.unarchive_chats(chat)
-            await msg.delete()
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'bot'))
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await client.unarchive_chats(chat)
-            done += 1
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'bot'))
-        except Exception as e:
-            return await msg.edit(bhs("text_error").format(em.gagal, e))
-    elif query.lower() == "ch":
-        try:
-            await client.unarchive_chats(chat)
-            done += 1
-            await msg.delete()
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'channel'))
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await client.unarchive_chats(chat)
-            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chat), em.keterangan, 'channel'))
-        except Exception as e:
-            return await msg.edit(bhs("text_error").format(em.gagal, e))
+            return await message.reply(bhs("arsip_out").format(em.berhasil, em.total, len(chats), em.keterangan, query))
+        except Exception as error:
+            return await msg.edit(bhs("text_error").format(em.gagal, error))
